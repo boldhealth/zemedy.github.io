d7e588575b9155d5215ca22d651d76c8c7adc0d8
Prince Abalogu <chineduabalogu@yahoo.com>
2019-12-16 00:25:09 +0100

    clean up code

--- js/components/sidebar/sidebar-component.js
+++ js/components/sidebar/sidebar-component.js
@@ -2,11 +2,6 @@ import { SidebarTemplate } from './sidebar-template.js'
 
 const SidebarComponent = {
   template: SidebarTemplate,
-  data: function(){
-    return {
-      removetoggle: true
-    }
-  },
   methods: {
     removeToggle: function(){
       this.$store.dispatch("removeToggle");
